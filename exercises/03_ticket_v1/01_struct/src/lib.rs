d91ed2683dc1a2370327ae2bfd7fc27ae3023ac7
mercydapp <mercydapps23@gmail.com>
2026-07-16 03:34:41 +0100

    completed 3 challenges

--- exercises/03_ticket_v1/01_struct/src/lib.rs
+++ exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -5,6 +5,17 @@
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
 
+struct Order {
+    price: u8,
+    quantity: u8
+}
+
+impl Order {
+    fn is_available(self) -> bool {
+        self.quantity > 0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,7 +24,7 @@ mod tests {
     fn test_order_is_available() {
         let order = Order {
             price: 100,
-            quantity: 10,
+            quantity: 10
         };
         assert!(order.is_available());
     }
@@ -22,7 +33,7 @@ mod tests {
     fn test_order_is_not_available() {
         let order = Order {
             price: 100,
-            quantity: 0,
+            quantity: 0
         };
         assert!(!order.is_available());
     }
